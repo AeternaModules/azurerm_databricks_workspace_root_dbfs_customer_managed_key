a3f0770019b2dad2bcb462c427d66d0c85cf7b76
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:01:47 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "databricks_workspace_root_dbfs_customer_managed_keys_id" {
+  description = "Map of id values across all databricks_workspace_root_dbfs_customer_managed_keys, keyed the same as var.databricks_workspace_root_dbfs_customer_managed_keys"
+  value       = { for k, v in azurerm_databricks_workspace_root_dbfs_customer_managed_key.databricks_workspace_root_dbfs_customer_managed_keys : k => v.id }
+}
 output "databricks_workspace_root_dbfs_customer_managed_keys_key_vault_id" {
   description = "Map of key_vault_id values across all databricks_workspace_root_dbfs_customer_managed_keys, keyed the same as var.databricks_workspace_root_dbfs_customer_managed_keys"
   value       = { for k, v in azurerm_databricks_workspace_root_dbfs_customer_managed_key.databricks_workspace_root_dbfs_customer_managed_keys : k => v.key_vault_id }
