230e62a767b884ce2aef0f3e353187bc2d91e26e
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:09 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "databricks_workspace_root_dbfs_customer_managed_keys" {
-  description = "All databricks_workspace_root_dbfs_customer_managed_key resources"
-  value       = azurerm_databricks_workspace_root_dbfs_customer_managed_key.databricks_workspace_root_dbfs_customer_managed_keys
-}
 output "databricks_workspace_root_dbfs_customer_managed_keys_key_vault_id" {
-  description = "List of key_vault_id values across all databricks_workspace_root_dbfs_customer_managed_keys"
-  value       = [for k, v in azurerm_databricks_workspace_root_dbfs_customer_managed_key.databricks_workspace_root_dbfs_customer_managed_keys : v.key_vault_id]
+  description = "Map of key_vault_id values across all databricks_workspace_root_dbfs_customer_managed_keys, keyed the same as var.databricks_workspace_root_dbfs_customer_managed_keys"
+  value       = { for k, v in azurerm_databricks_workspace_root_dbfs_customer_managed_key.databricks_workspace_root_dbfs_customer_managed_keys : k => v.key_vault_id }
 }
 output "databricks_workspace_root_dbfs_customer_managed_keys_key_vault_key_id" {
-  description = "List of key_vault_key_id values across all databricks_workspace_root_dbfs_customer_managed_keys"
-  value       = [for k, v in azurerm_databricks_workspace_root_dbfs_customer_managed_key.databricks_workspace_root_dbfs_customer_managed_keys : v.key_vault_key_id]
+  description = "Map of key_vault_key_id values across all databricks_workspace_root_dbfs_customer_managed_keys, keyed the same as var.databricks_workspace_root_dbfs_customer_managed_keys"
+  value       = { for k, v in azurerm_databricks_workspace_root_dbfs_customer_managed_key.databricks_workspace_root_dbfs_customer_managed_keys : k => v.key_vault_key_id }
 }
 output "databricks_workspace_root_dbfs_customer_managed_keys_workspace_id" {
-  description = "List of workspace_id values across all databricks_workspace_root_dbfs_customer_managed_keys"
-  value       = [for k, v in azurerm_databricks_workspace_root_dbfs_customer_managed_key.databricks_workspace_root_dbfs_customer_managed_keys : v.workspace_id]
+  description = "Map of workspace_id values across all databricks_workspace_root_dbfs_customer_managed_keys, keyed the same as var.databricks_workspace_root_dbfs_customer_managed_keys"
+  value       = { for k, v in azurerm_databricks_workspace_root_dbfs_customer_managed_key.databricks_workspace_root_dbfs_customer_managed_keys : k => v.workspace_id }
 }
 
