84a75f4c7fb847228f19cc441b6480f10c1f1aab
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:47:42 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 output "databricks_workspace_root_dbfs_customer_managed_keys_id" {
   description = "Map of id values across all databricks_workspace_root_dbfs_customer_managed_keys, keyed the same as var.databricks_workspace_root_dbfs_customer_managed_keys"
-  value       = { for k, v in azurerm_databricks_workspace_root_dbfs_customer_managed_key.databricks_workspace_root_dbfs_customer_managed_keys : k => v.id }
+  value       = { for k, v in azurerm_databricks_workspace_root_dbfs_customer_managed_key.databricks_workspace_root_dbfs_customer_managed_keys : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "databricks_workspace_root_dbfs_customer_managed_keys_key_vault_id" {
   description = "Map of key_vault_id values across all databricks_workspace_root_dbfs_customer_managed_keys, keyed the same as var.databricks_workspace_root_dbfs_customer_managed_keys"
-  value       = { for k, v in azurerm_databricks_workspace_root_dbfs_customer_managed_key.databricks_workspace_root_dbfs_customer_managed_keys : k => v.key_vault_id }
+  value       = { for k, v in azurerm_databricks_workspace_root_dbfs_customer_managed_key.databricks_workspace_root_dbfs_customer_managed_keys : k => v.key_vault_id if v.key_vault_id != null && length(v.key_vault_id) > 0 }
 }
 output "databricks_workspace_root_dbfs_customer_managed_keys_key_vault_key_id" {
   description = "Map of key_vault_key_id values across all databricks_workspace_root_dbfs_customer_managed_keys, keyed the same as var.databricks_workspace_root_dbfs_customer_managed_keys"
-  value       = { for k, v in azurerm_databricks_workspace_root_dbfs_customer_managed_key.databricks_workspace_root_dbfs_customer_managed_keys : k => v.key_vault_key_id }
+  value       = { for k, v in azurerm_databricks_workspace_root_dbfs_customer_managed_key.databricks_workspace_root_dbfs_customer_managed_keys : k => v.key_vault_key_id if v.key_vault_key_id != null && length(v.key_vault_key_id) > 0 }
 }
 output "databricks_workspace_root_dbfs_customer_managed_keys_workspace_id" {
   description = "Map of workspace_id values across all databricks_workspace_root_dbfs_customer_managed_keys, keyed the same as var.databricks_workspace_root_dbfs_customer_managed_keys"
-  value       = { for k, v in azurerm_databricks_workspace_root_dbfs_customer_managed_key.databricks_workspace_root_dbfs_customer_managed_keys : k => v.workspace_id }
+  value       = { for k, v in azurerm_databricks_workspace_root_dbfs_customer_managed_key.databricks_workspace_root_dbfs_customer_managed_keys : k => v.workspace_id if v.workspace_id != null && length(v.workspace_id) > 0 }
 }
 
